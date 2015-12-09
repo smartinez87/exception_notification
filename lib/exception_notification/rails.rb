a25b106e0c1fed76e568d7b6e79b932913c1e417
Seamus Abshere <seamus@abshere.net>
2015-12-08 19:17:15 -0500

    #wip get a real connection pool error, don't know how to test it yet

--- lib/exception_notification/rails.rb
+++ lib/exception_notification/rails.rb
@@ -3,6 +3,7 @@ module ExceptionNotification
     config.exception_notification = ExceptionNotifier
     config.exception_notification.logger = Rails.logger
 
-    config.app_middleware.use ExceptionNotification::Rack
+    # config.app_middleware.use ExceptionNotification::Rack
+    config.app_middleware.insert_before ActiveRecord::ConnectionAdapters::ConnectionManagement, ExceptionNotification::Rack
   end
 end
